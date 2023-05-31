aa65a5e17680beb7857602264937eaf08aa9a680
Hannele Ruiz <justlemoncl@gmail.com>
2023-05-31 07:25:21 -0400

    Added function to load a resource language file

--- client.lua
+++ client.lua
@@ -12,5 +12,26 @@ local function getCurrentLanguage()
     end
 end
 
+local function loadLanguageForResource(resource, language)
+    local file = "i18n" .. "/" .. language .. ".json"
+    local contents = LoadResourceFile(resource, file)
+
+    if contents == nil then
+        print("Language " .. language .. " was not found")
+        Debug("File " .. file .. " for language " .. language .. " is missing")
+        return nil
+    end
+
+    local parsed = json.decode(contents)
+
+    if type(parsed) == "table" then
+        return parsed
+    else
+        print("Invalid language file for " .. language)
+        Debug("Expected table for language file " .. file .. ", got " .. type(parsed))
+        return nil
+    end
+end
+
 exports("getCurrentLocale", getCurrentLocale)
 exports("getCurrentLanguage", getCurrentLanguage)

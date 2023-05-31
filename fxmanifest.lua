4145991eee9a6820e4bce87338fd560d9c46ed9d
Hannele Ruiz <justlemoncl@gmail.com>
2023-05-31 10:09:30 -0400

    Moved language, locale and loader functions to a shared file

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,9 @@ fx_version "cerulean"
 games { "gta5" }
 
 client_script "common.lua"
+client_script "shared.lua"
 client_script "client.lua"
 
 server_script "common.lua"
+server_script "shared.lua"
 server_script "server.lua"

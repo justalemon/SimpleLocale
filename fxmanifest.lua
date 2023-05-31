73b2d1e44a92339e7b8ea02f2fe61edf510bcb8e
Hannele Ruiz <justlemoncl@gmail.com>
2023-05-31 06:15:22 -0400

    Renamed shared.lua to common.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,8 @@
 fx_version "cerulean"
 games { "gta5" }
 
-client_script "shared.lua"
+client_script "common.lua"
 client_script "client.lua"
-server_script "shared.lua"
+
+server_script "common.lua"
 server_script "server.lua"

5092bb82a1cd6e701c04b108241b2026d8925498
Hannele Ruiz <justlemoncl@gmail.com>
2023-05-31 06:08:01 -0400

    Fixed loading order of the lua files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version "cerulean"
 games { "gta5" }
 
-client_script "client.lua"
 client_script "shared.lua"
-server_script "server.lua"
+client_script "client.lua"
 server_script "shared.lua"
+server_script "server.lua"
